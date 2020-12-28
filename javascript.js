19a5457d21eb2e39c17debc58004929daf32b7e5
hirei <activeage03@icloud.com>
2020-12-28 17:44:11 +0900

    second commit

--- javascript.js
+++ javascript.js
@@ -1,27 +1,38 @@
-function showNowTime(){
-    var nd = new Date();
-    alert(nd);
-    var year = nd.getFullYear();
-    var month = ("0"+(nd.getMonth()+1)).slice(-2);
-    var date = ("0"+nd.getDate()).slice(-2);
-    var hours = ("0"+nd.getHours()).slice(-2);
-    var minutes = ("0"+nd.getMinutes()).slice(-2);
-    var seconds = ("0"+nd.getSeconds()).slice(-2);
+// 時計のメインとなる関数
+function clock()
+{
+    // 曜日を表す各文字列の配列
+    var weeks = new Array("Sun","Mon","Thu","Wed","Thr","Fri","Sat");
+    // 現在日時を表すインスタンスを取得
+    var now = new Date();
+    // 年
+    var y = now.getFullYear();
+    // 月 0~11で取得されるので実際の月は+1したものとなる
+    var mo = now.getMonth() + 1;
+    // 日
+    var d = now.getDate();
+    // 曜日 0~6で日曜始まりで取得されるのでweeks配列のインデックスとして指定する
+    var w = weeks[now.getDay()];
+    // 時
+    var h = now.getHours();
+    // 分
+    var mi = now.getMinutes();
+    // 秒
+    var s = now.getSeconds();
 
-    alert(year+"年"+month+"月"+date+"日"+hours+"時"+minutes+"分"+seconds+"秒");
+    // 日付時刻文字列のなかで常に2ケタにしておきたい部分はここで処理
+    if (mo < 10) mo = "0" + mo;
+    if (d < 10) d = "0" + d;
+    if (mi < 10) mi = "0" + mi;
+    if (s < 10) s = "0" + s;
+
+    //　HTML: <span id="clock_date">(ココの日付文字列を書き換え)</span>
+    document.getElementById("clock_date").innerHTML =  y + "/" + mo + "/" + d + " (" + w + ")";
+    //　HTML: <span id="clock_time">(ココの時刻文字列を書き換え)</span>
+    document.getElementById("clock_time").innerHTML = h + ":" + mi + ":" + s;
+    //　HTML: <div id="clock_frame"> の内部要素のフォントサイズをウインドウサイズの10分の1ピクセルに設定
+    document.getElementById("clock_frame").style.fontSize =  window.innerWidth / 10 + "px";
 }
 
-function showLastModified(){
-    var mod = document.lastModified;
-    alert(mod);
-    var mod_arr = mod.split(" ")[0].split("/");
-    mod_arr = mod_arr.concat(mod.split(" ")[1].split(":"));
-    var year = mod_arr[2];
-    var month = mod_arr[0];
-    var date = mod_arr[1];
-    var hours = mod_arr[3];
-    var minutes = mod_arr[4];
-    var seconds = mod_arr[5];
-
-    alert(year+"年"+month+"月"+date+"日"+hours+"時"+minutes+"分"+seconds+"秒");
-}
+// 上記のclock関数を1000ミリ秒ごと(毎秒)に実行する
+setInterval(clock, 1000);
